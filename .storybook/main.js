862f7680d784e266fa92c100bbc9fc993d40f08a
maurodollinger-globant <mauro.dollinger@globant.com>
2024-09-17 11:55:16 -0300

    Create main.js

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/html-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+  ],
+  framework: {
+    name: '@storybook/html-webpack5',
+    options: {},
+  },
+};
+export default config;
